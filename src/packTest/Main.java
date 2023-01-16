e4221210ffbdc2f85c1636db3b592e826eb0f34b
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2023-01-16 12:11:46 +0200

    Updated Main

--- src/packTest/Main.java
+++ src/packTest/Main.java
@@ -1,11 +1,10 @@
 package packTest;
 
-import packWork.ProcessImage;
-import packWork.ReadImage1;
-import packWork.WriteImage1;
+import packWork.*;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -19,17 +18,72 @@ public class Main {
         long elapsedTime = System.nanoTime() - startTime;
         System.out.println("Total execution time to identify source and destination image: " + elapsedTime + " ns");
 
-        BufferedImage sourceImage = null;
-        BufferedImage enlargedImage = null;
+        BufferedImage sourceImage;
+        BufferedImage enlargedImage;
 
-        ReadImage1 read = new ReadImage1();
-        sourceImage = read.readImage();
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Choose what image you want to enlarge (1, 2, 0 for exit): ");
+        int inputImage = keyboard.nextInt();
 
-        ProcessImage process = new ProcessImage();
-        enlargedImage = process.processImage(sourceImage, 2);
+        keyboard = new Scanner(System.in);
+        System.out.println("Choose how many times you want to enlarge the image: ");
+        int inputResize = keyboard.nextInt();
 
-        WriteImage1 write = new WriteImage1();
-        WriteImage1.writeImage(enlargedImage);
+        while(inputImage != 0) {
+            switch(inputImage) {
+                case 0:
+                    break;
+                case 1:
+                    ReadImage1 read1 = new ReadImage1();
+                    sourceImage = read1.readImage();
+
+                    ProcessImage process1 = new ProcessImage();
+                    enlargedImage = process1.processImage(sourceImage, inputResize);
+
+                    WriteImage1 write1 = new WriteImage1();
+                    write1.writeImage(enlargedImage);
+
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose what image you want resized (1, 2, 0 for exit): ");
+                    inputImage = keyboard.nextInt();
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose how many times you want to enlarge the image: ");
+                    inputResize = keyboard.nextInt();
+                    break;
+                case 2:
+                    ReadImage2 read2 = new ReadImage2();
+                    sourceImage = read2.readImage();
+
+                    ProcessImage process2 = new ProcessImage();
+                    enlargedImage = process2.processImage(sourceImage, inputImage);
+
+                    WriteImage2 write2 = new WriteImage2();
+                    write2.writeImage(enlargedImage);
+
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose what image you want resized (1, 2, 0 for exit): ");
+                    inputImage = keyboard.nextInt();
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose what image you want resized (1, 2, 0 for exit): ");
+                    inputImage = keyboard.nextInt();
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose how many times you want to enlarge the image: ");
+                    inputResize = keyboard.nextInt();
+                    break;
+                default:
+                    System.out.println("Invalid input.");
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose what image you want resized (1, 2, 0 for exit): ");
+                    inputImage = keyboard.nextInt();
+                    keyboard = new Scanner(System.in);
+                    System.out.println("Choose how many times you want to enlarge the image: ");
+                    inputResize = keyboard.nextInt();
+            }
+        }
+
+
+
+
 
     }
 }
